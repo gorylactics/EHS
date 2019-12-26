f0f90820c050fecba204e752da6120bfc2863590
adrian <adrian.miranda.r@gmail.com>
2019-12-25 23:13:37 -0300

    prueba de contador

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,27 @@
-window.onload = function(){
-    
 // alert("El HOmbre seguro , vino lo hizo y lo que hizo lo hizo sentir Bien")
+window.onload = function(){
 
-var titulo = document.getElementById("titulo");
-var boton = document.getElementById("boton");
+    function cambioTitulo(){
+        var titulo = document.querySelector("#titulo");
+        var boton = document.querySelector("#boton");
+        var nombre = prompt('introduce tu nombre');
+        boton.addEventListener('click' , cambioTitulo);
+        titulo.innerHTML= 'Bienvenido ' + nombre;
+        // boton.addEventListener('click' , cambioTitulo); 
+        // fuera de la funcion y con el llamado apagado , solo al pulsar el boton
+    };
+        
+        // cambioTitulo();
 
+    function likes(){
+        var contador = document.querySelector('#contador');
+        var masUno = document.querySelector('#botonMas');
+        var menosUno = document.querySelector('#botonMenos');
+        
+        
+    };
 
-function cambioTitulo(){
-    var nombre = prompt('introduce tu nombre');
-    titulo.innerHTML= nombre;
-};
-
-boton.addEventListener('click' , cambioTitulo);
-
-
+    likes();
 };
 
 
